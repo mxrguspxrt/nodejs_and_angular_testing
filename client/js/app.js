80c419a00d250b20448ab817c81f77c4a67afbad
Margus Pärt <margus@wave.ee>
2014-12-21 04:44:53 +0200

    removed example data

--- client/js/app.js
+++ client/js/app.js
@@ -10,14 +10,6 @@ var App = angular.module('App', [
 App.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/phones', {
-        templateUrl: 'templates/phones/index.html',
-        controller: 'PhonesIndexController'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'templates/phones/show.html',
-        controller: 'PhonesShowController'
-      }).
       when("/images/:date", {
         templateUrl: "templates/comicimages/index.html",
         controller: "ComicimagesIndexController"
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,20 +1,5 @@
 var AppControllers = angular.module('AppControllers', []);
 
-AppControllers.controller('PhonesIndexController', ['$scope', 'Phone', function($scope, Phone) {
-  $scope.phones = Phone.query();
-  $scope.orderProp = 'age';
-}]);
-
-
-AppControllers.controller('PhonesShowController', ['$scope', '$routeParams', 'Phone', function($scope, $routeParams, Phone) {
-  $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-    $scope.mainImageUrl = phone.images[0];
-  });
-
-  $scope.setImage = function(imageUrl) {
-    $scope.mainImageUrl = imageUrl;
-  }
-}]);
 
 AppControllers.controller('ComicimagesIndexController', ['$scope', '$routeParams', 'Comicimage', function($scope, $routeParams, Comicimage) {
 
@@ -25,9 +10,6 @@ AppControllers.controller('ComicimagesIndexController', ['$scope', '$routeParams
   var currentDate = $routeParams.date == "today" ? moment() : moment($routeParams.date, dateFormat);
   var previousDate = moment(currentDate).subtract(1, "day");
   var nextDate = moment(currentDate).add(1, "day");
-
-  alert(currentDate);
-  alert(previousDate);
 
   $scope.currentDateString = currentDate.format(dateFormat);
   $scope.previousDateString = previousDate.format(dateFormat);
